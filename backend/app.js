7ca6fbdd06d19a100c33b25d093202b725fef00f
FernandaVitro <fernandarafaelavitro@gmail.com>
2026-03-19 11:02:57 -0300

    swagger 19/03

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,11 @@ const porta = 3000
 const app = express()
 app.use(cors())
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 
 const conexao = require("./db.js")
 
